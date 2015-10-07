98b0bf34ec7d5adb5416c2265def6cf3370e92b7
Dheeraj Prasanth <dheeraj.prasanth@gmail.com>
2015-10-08 00:30:42 +0530

    Single page app to serve the articles completed

--- blog.js
+++ blog.js
@@ -5,6 +5,18 @@ if (Meteor.isServer) {
             return Meteor.http.get("http://localhost:8000/articles/", {timeout:30000});
         }
     });
+    Meteor.methods({
+        randomContent: function () {
+            this.unblock();
+            return Meteor.http.get("http://localhost:8000/random/", {timeout:30000});
+        }
+    });
+    Meteor.methods({
+        getSpecificArticle: function (id) {
+            this.unblock();
+            return Meteor.http.get("http://localhost:8000/articles/" + id, {timeout:30000});
+        }
+    });
 }
 
 if (Meteor.isClient) {
@@ -13,24 +25,40 @@ if (Meteor.isClient) {
          Session.set('articles', JSON.parse(results.content).articles);
     });
 
-  // This code only runs on the client
+    Meteor.call("randomContent", function(error, results){
+        Session.set('mainarticle', JSON.parse(results.content).article);
+        Session.set('images', JSON.parse(results.content).images);
+        Session.set('specific', null);
+    })
+
   Template.body.helpers({
-      images: [
-        { url: "/static/google.png" },
-        { url: "/static/google.png" },
-        { url: "/static/google.png" },
-        { url: "/static/google.png" }
-      ],
+      images: function(){
+        return Session.get('images');
+      }
+  });
+
+  // This code only runs on the client
+  Template.main.helpers({
       articles: function(){
         return Session.get('articles');
       },
+      mainarticles: function(){
+        return Session.get('mainarticle');
+      },
+      specific: function(){
+        return Session.get('specific');
+      }
   });
 
   Template.body.events({
     "click .article-content": function(event){
       event.preventDefault();
       var id = event.currentTarget.id;
-      console.log(id);
+      Session.set('specific', true);
+      Session.set('articles', null);
+      Meteor.call("getSpecificArticle", id, function(error, results) {
+         Session.set('mainarticle', JSON.parse(results.content).article);
+      });
     }
   });
 }
